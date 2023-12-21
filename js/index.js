ecc7977e7cc31705e679fd2ebf740f0567fe88fa
farid <farid.mouzoun@outlook.fr>
2023-12-21 11:20:35 +0100

    modifs
    modifs

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ var nbr = parseInt(prompt("Nombre initial entre 1 et 10"));
 if (nbr > 10) {
     alert("Le nombre est supérieur à 10, je vous ai demandé d'écrire un nombre compris entre 1 et 10!!!");
 } else {
-    for (var i = nbr; i <= 10; i++) {
+    var tour10 = nbr + 10;
+    for (var i = nbr; i <= tour10; i++) {
         if (i % 2 === 0) {
         console.log(i + " est pair");
         } else {
